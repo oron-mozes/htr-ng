5e181a5c7be1838221cdb2ba601ef3a38f655b77
mozesoron <oronm@wix.com>
2021-10-31 20:18:47 +0200

    express

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static(__dirname + '/dist/FirstWebComponent'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/dist/FirstWebComponent/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
